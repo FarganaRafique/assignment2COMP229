37a7d3b6ce31c7eff8724b0b8f6cedcef4db818d
FarganaRafique <farganarafique@gmail.com>
2020-10-25 23:02:33 -0400

    updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports =
 {
-    "URI":"mongodb://localhost/business_contacts"
-    //"URI": "",
+    //"URI":"mongodb://localhost/business_contacts"
+    "URI": "mongodb+srv://fargana:qmhB9QvZWdlAl7TZ@mongodbserver.xdsvo.mongodb.net/business_contacts?retryWrites=true&w=majority"
     //"Secret":'SomeSecret'
 }
